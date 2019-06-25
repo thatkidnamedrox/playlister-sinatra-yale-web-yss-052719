af19c51a7e6c8ac3455a9948838d3a7e854d089f
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-25 17:22:09 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,12 @@
 class ArtistsController < ApplicationController
+  get '/artists' do
+    @artists = Artist.all
+    erb :index
+  end
+
+  # show page
+  get '/artists/:slug' do
+
+  end
+
 end
---
+++ app/models/artist.rb
@@ -0,0 +1,32 @@
+class Artist < ActiveRecord::Base
+  has_many :songs
+  has_many :genres, through: :songs
+
+  def slug
+    #strip the string
+    ret = self.name.strip
+
+    #blow away apostrophes
+    ret.gsub! /['`]/,""
+
+    # @ --> at, and & --> and
+    ret.gsub! /\s*@\s*/, " at "
+    ret.gsub! /\s*&\s*/, " and "
+
+    #replace all non alphanumeric, underscore or periods with underscore
+     ret.gsub! /\s*[^A-Za-z0-9\.\-]\s*/, '-'
+
+     #convert double underscores to single
+     ret.gsub! /-+/,"-"
+
+     #strip off leading/trailing underscore
+     ret.gsub! /\A[-\.]+|[-\.]+\z/,""
+
+     ret
+  end
+
+  def self.find_by_slug(slug)
+    self.find_by "name.slug == ?", slug
+  end
+
+end
